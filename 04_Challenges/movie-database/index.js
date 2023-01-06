7d23663dac16eb64b65a0e0ac71bfb68abadf932
alaahj8 <hij.alaa8@gmail.com>
2023-01-06 11:46:22 -0500

    step3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -9,3 +9,45 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
 })
+
+app.get('/search',(req,res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        // Search string applied
+        const response = {
+            status:200, message:"ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
+
+
+        res.status(500);
+        res.send(response);
+    }
+});
+app.get('/search',(req,res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        // Search string applied
+        const response = {status:200, message: TIME, data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
+
+
+        res.status(500);
+        res.send(response);
+    }
+});

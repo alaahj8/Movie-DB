6068bd36a7cc2d663eae7c566a61d13faf8532a6
alaahj8 <hij.alaa8@gmail.com>
2023-01-07 12:16:51 -0500

    step13

--- 04_Challenges/movie-database/server.js
+++ 04_Challenges/movie-database/server.js
@@ -23,3 +23,29 @@ var nameSchema = new mongoose.Schema({
         res.status(400).send("unable to save to database");
       });
   });  
+  app.post("/username", (req, res) => {
+ 
+  });
+  app.post("/username", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+      .then(item => {
+        res.send("item saved to database");
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+  });  
+  app.post("/password", (req, res) => {
+ 
+  });
+  app.post("/password", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+      .then(item => {
+        res.send("item saved to database");
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+  });  

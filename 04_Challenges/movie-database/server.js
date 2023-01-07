ed30af3c864c0ab0784a2497608aabed526115a3
alaahj8 <hij.alaa8@gmail.com>
2023-01-07 12:13:14 -0500

    step12

---
+++ 04_Challenges/movie-database/server.js
@@ -0,0 +1,25 @@
+const express        = require('express');const MongoClient    = require('mongodb').MongoClient;const bodyParser     = require('body-parser');
+const app            = express();
+const port = 8000;
+app.listen(port, () => {  console.log('We are live on ' + port);});
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastNameName: String
+  });
+  app.post("/addname", (req, res) => {
+ 
+  });
+  
+  app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+      .then(item => {
+        res.send("item saved to database");
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+  });  
